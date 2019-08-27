196581f80b0694ecaf1d86d7dd8e9de4e92e9434
maltebp <maltebp94@gmail.com>
2019-08-27 13:13:58 +0200

    Re added deleted file

--- src/test/java/SerializeLevel.java
+++ src/test/java/SerializeLevel.java
@@ -24,8 +24,6 @@ public class SerializeLevel {
 
         Level inputLevel;
 
-        //REMOVE THIS
-
 
         try {
             ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(FILENAME));
